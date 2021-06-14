ebc6299de7572c376c13a325c6ca13c92ea41d1d
kenzo7664 <olaotanemmanuel1@gmail.com>
2021-06-14 06:00:01 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,85 @@
+const form = document.getElementById("form");
+const fname = document.getElementById("fname");
+const lname = document.getElementById("lname");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    checkInputs();
+
+    
+});
+
+function checkInputs() {
+    const fnameValue = fname.value.trim()
+    const lnameValue = lname.value.trim()
+    const emailValue = email.value.trim()
+    const passwordValue = password.value.trim()
+    const password2Value = password2.value.trim()
+
+    if (fnameValue == "") {
+
+
+       setErrorFor(fname, "First name cannot be blank");
+    }else {
+        setSuccessFor(fname);
+    }
+
+    if (lnameValue == "") {
+        setErrorFor(lname, "Last name cannot be blank");
+    }else {
+        setSuccessFor(lname);
+    }
+
+    if (emailValue == "") {
+        setErrorFor(email, "Email cannot be blank");
+    }else if(!isEmail(emailValue)){
+        setErrorFor(email, "Not a valid email");
+    }else {
+        setSuccessFor(email);
+    }
+
+    if (passwordValue == "") {
+        setErrorFor(password, "Password cannot be blank");
+    }else {
+        setSuccessFor(password);
+    }
+
+    if (password2Value == "") {
+        setErrorFor(password2, "Password2 cannot be blank");
+    }else if(passwordValue !== password2Value) {
+        setErrorFor(password2, "Passwords does not match");
+    }else{
+        setSuccessFor(password2);
+    }
+
+    if (document.querySelectorAll('.form-control')[0].classList.contains('success') && 
+       document.querySelectorAll('.form-control')[1].classList.contains('success') && 
+       document.querySelectorAll('.form-control')[2].classList.contains('success') &&
+       document.querySelectorAll('.form-control')[3].classList.contains('success') &&
+       document.querySelectorAll('.form-control')[4].classList.contains('success') ) {
+        form.submit()
+    }
+
+}
+
+function  setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small");
+    formControl.className = "form-control error";
+    small.innerText = message;
+
+    
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
+
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
